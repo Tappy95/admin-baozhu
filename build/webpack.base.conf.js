804d958231f1225f63bee59431ef0f09b988fe61
陆荣琴 <18798802609@163.com>
2019-06-17 16:31:24 +0800

    cdn加速

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -86,5 +86,10 @@ module.exports = {
     net: 'empty',
     tls: 'empty',
     child_process: 'empty'
+  },
+  externals: {
+    'vue': 'Vue',
+    'vue-router': 'VueRouter',
+    'element-ui': 'ELEMENT',
   }
 }
